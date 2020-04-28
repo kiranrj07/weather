b94cfe9f85cbcfd68873fae76920a89e083347aa
kiranrj07 <kiranrj07@gmail.com>
2020-04-29 00:46:03 +0530

    remove unecessary space

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (long, lat, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-
             callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
