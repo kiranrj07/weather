fe2ab10ecaaa58b0cfab93b862c6eea0f630448f
kiranrj07 <kiranrj07@gmail.com>
2020-10-19 16:01:35 +0530

    Final changes

--- src/app.js
+++ src/app.js
@@ -29,14 +29,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Kiran Raj'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Kiran Raj'
     })
 })
 
@@ -44,7 +44,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Kiran Raj'
     })
 })
 
